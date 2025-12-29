5a2aac0d84a46ed5f888df285335a170aab40323
giantessicini <39560686+giantessicini@users.noreply.github.com>
2025-12-29 19:09:47 -0300

    feat: Push notifications Firebase Message setup

---
+++ Assets/Scripts/PushNotifications/PushNotifications.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using Firebase;
+using Firebase.Extensions;
+using Firebase.Messaging;
+using UnityEngine.Android;
+
+public class PushNotifications : MonoBehaviour
+{
+    [SerializeField] private GameObject successPanel, failurePanel;
+
+    public void Start()
+    {
+        if (!Permission.HasUserAuthorizedPermission("android.permission.POST_NOTIFICATIONS"))
+        {
+            Permission.RequestUserPermission("android.permission.POST_NOTIFICATIONS");
+        }
+
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                Firebase.FirebaseApp app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                successPanel.SetActive(true);
+
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+                failurePanel.SetActive(true);
+            }
+        });
+
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+    }
+
+    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
+    }
+
+    public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    }
+}
